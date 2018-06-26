3240506f22dcf46680f15f7142fdb7f7de978e16
Baggio Shehadi <k3nh1n@yahoo.com>
2018-06-26 18:52:15 -0500

    function2

--- func.js
+++ func.js
@@ -10,8 +10,23 @@ function getClothing(isCold){
 		//declare hot
 		var hot = "shorts";
 		//undefine because true statement is never ran also block scope
-		console.log(freezing);
+		// console.log(freezing);
 	}
 }
 //runs getclothing function with false as the parameter sent
 getClothing(false);
+
+
+//a way to avoid this:
+function getClothing2(isCold){
+	//let works like most variable declaration in other languages where it limits it to block scope as opposed to  var which is global.
+	let freezing = "jacket"
+	if(isCold){
+
+	}else{
+		let freezing = "shorts"
+		//console logs shorts
+		console.log(freezing);
+	}
+}
+getClothing2(false);

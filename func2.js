f65e3c22367fe18ce75ca18a2c144aedcee16e35
Baggio Shehadi <k3nh1n@yahoo.com>
2018-06-26 19:07:27 -0500

    added 2nd function

---
+++ func2.js
@@ -0,0 +1,19 @@
+const locations = [
+	"Brentwood",
+	"CA",
+	"TX",
+	"Santa Monica",
+	"Oregon",
+	"Washington",
+	"Arizona",
+	"Venice",
+	"Utah"
+];
+const [location_one,stateOne,stateTwo,location_two,stateThree,stateFour,stateFive,location_three,stateSix] = locations;
+
+const cities = `List of Cities
+1. ${location_one}
+2. ${location_two}
+3. ${location_three}
+`
+console.log(cities);
